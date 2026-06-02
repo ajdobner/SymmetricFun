19c002dae7711c522bdecaa6bbd9c06263452470
Alex Dobner <alexander.dobner@gmail.com>
2026-06-02 14:10:17 -0400

    Added definitions and goals

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-# Symmetric Polynomials
+# SymmetricFun
 
-## GitHub configuration
+## Goal
 
-To set up your new GitHub repository, follow these steps:
+Implement the ring of symmetric functions in Lean and prove some of its properties.
+* Some facts about symmetric polynomials are implemented in `mathlib`. These only work for finitely many variables. The infinite variable case requires taking some kind of categorical limit or using subalgebras of power series rings.
+* Currently `mathlib` implements multivariate polynomial rings $R[x_i : i \in I]$ using a general monoid algebra construction. Symmetric polynomials are then implemented as a subalgebra. Power series rings $R[[x_i : i \in I]]$ are also implemented, but not using any general machinery. The latter could be generalized via the **total monoid algebra** construction $R[[M]]$, which is not implemented in Lean.
 
-* Under your repository name, click **Settings**.
-* In the **Actions** section of the sidebar, click "General".
-* Check the box **Allow GitHub Actions to create and approve pull requests**.
-* Click the **Pages** section of the settings sidebar.
-* In the **Source** dropdown menu, select "GitHub Actions".
+**Plan**
+1. Initially construct the ring of symmetric functions (and polynomials) as a subalgebra of the existing `MvPowerSeries` ring in `mathlib`.
+2. Define various bases for the ring of symmetric functions: monomial, elementary, complete homogeneous, Schur, power sum.
+    * For Schur functions this requires choosing one of the definitions. Probably it's best to use semistandard Young tableau definition?
+3. Prove the fundamental theorem of symmetric functions. This is proved already in the finite variable case.
+4. Define the Hall inner product and the $\omega$ involution.
+4. Prove the Pieri rules. 
+4. (Later?) Refactor `MvPowerSeries` to use the total monoid algebra construction. One advantage here is that *specialization* in power series (or polynomial) rings is an instance of a more general construction: if $M \to N$ is a monoid homomorphism then this induces a homomorphism $R[[M]]\to R[[N]]$. Specialization is useful for proving the categorical limit properties of symmetric functions.
+4. (Hard) Prove positivity of Littlewood--Richardson coefficients, and/or the Littlewood--Richardson rule. Representation theory of $S_n$ and/or $GL_n$ are not implemented in `mathlib` so combinatorial proofs are probably better anyway.
 
-After following the steps above, you can remove this section from the README file.
--- SymmetricPoly/SymmetricFun.lean
+++ SymmetricPoly/SymmetricFun.lean
@@ -1,25 +1,60 @@
-import SymmetricPoly.DecomposableAddMonoid
-import Mathlib.Data.Finsupp.Defs
-import Mathlib.Data.Finsupp.Antidiagonal
+import Mathlib.RingTheory.MvPowerSeries.Basic
+import Mathlib.RingTheory.MvPowerSeries.Rename
+import Mathlib.Algebra.Algebra.Subalgebra.Basic
 
 noncomputable section
 
-instance {σ : Type*} [DecidableEq σ] : DecomposableAddMonoid (σ →₀ ℕ) where
-  antidiagonal m := m.antidiagonal'.support
-  mem_antidiagonal {m} {p} := by
-    rcases p with ⟨p₁, p₂⟩
-    simp [Finsupp.antidiagonal', ← and_assoc, Multiset.toFinsupp_eq_iff,
-      ← Multiset.toFinsupp_eq_iff (f:=m)]
+-- instance {σ : Type*} [DecidableEq σ] : DecomposableAddMonoid (σ →₀ ℕ) where
+--   antidiagonal m := m.antidiagonal'.support
+--   mem_antidiagonal {m} {p} := by
+--     rcases p with ⟨p₁, p₂⟩
+--     simp [Finsupp.antidiagonal', ← and_assoc, Multiset.toFinsupp_eq_iff,
+--       ← Multiset.toFinsupp_eq_iff (f:=m)]
 
 
--- namespace MvPowerSeries
+namespace MvPowerSeries
 
--- def IsBddDeg [CommSemiring R] (f : MvPowerSeries σ R) : Prop :=
---   ∃ d : ℕ, ∀ m : σ →₀ ℕ, f.coeff m ≠ 0 → m.sum (fun _ n => n) ≤ d
+variable {σ : Type*} {R : Type*} [CommSemiring R]
 
--- def boundedSubalgebra (σ R : Type*) [CommSemiring R] : Subalgebra R
---   (MvPowerSeries σ R) where
---   carrier := {f | IsBddDeg f}
+def totalDegree (p : MvPowerSeries σ R) : ℕ∞ :=
+  ⨆ (m : {m : σ →₀ ℕ // coeff m p ≠ 0}), (m.1.sum (fun _ n => n) : ℕ∞)
+
+theorem totalDegree_C (r : R) : (C r : MvPowerSeries σ R).totalDegree = 0 := by
+  sorry
+
+theorem totalDegree_mul (p q : MvPowerSeries σ R) :
+    totalDegree (p * q) ≤ totalDegree p + totalDegree q := by
+  sorry
+
+theorem totalDegree_add (p q : MvPowerSeries σ R) :
+    totalDegree (p + q) ≤ max (totalDegree p) (totalDegree q) := by
+  sorry
+
+def IsBddDeg (p : MvPowerSeries σ R) : Prop := totalDegree p < ⊤
+
+lemma isBddDeg_iff_exists_bound {σ R : Type*} [CommSemiring R] (p : MvPowerSeries σ R) :
+    IsBddDeg p ↔ ∃ (D : ℕ), ∀ m, p.coeff m ≠ 0 → m.sum (fun _ e => e) ≤ D := by sorry
+
+def bddDegSubalgebra (σ R : Type*) [CommRing R] : Subalgebra R (MvPowerSeries σ R) where
+  carrier := {f | IsBddDeg f}
+  algebraMap_mem' r := by
+    simp [IsBddDeg, algebraMap_apply, totalDegree_C]
+  add_mem' ha hb := by
+    rename_i f g
+    exact lt_of_le_of_lt (totalDegree_add f g) (max_lt ha hb)
+  mul_mem' ha hb := by
+    rename_i f g
+    exact lt_of_le_of_lt (totalDegree_mul f g) (WithTop.add_lt_top.mpr ⟨ha, hb⟩)
+
+def symmSubalgebra (σ R : Type*) [CommRing R] : Subalgebra R (MvPowerSeries σ R) where
+  carrier := {f | ∀ e : Equiv.Perm σ, rename e f = f}
+  algebraMap_mem' r e := rename_C e r
+  mul_mem' ha hb e := by rw [map_mul, ha, hb]
+  add_mem' ha hb e := by rw [map_add, ha, hb]
 
 
--- end MvPowerSeries
+def symmetricFunctions (σ R : Type*) [CommRing R] : Subalgebra R (MvPowerSeries σ R) :=
+  bddDegSubalgebra σ R ⊓ symmSubalgebra σ R
+
+
+end MvPowerSeries
